c4656697ff735734ebf84339c427508b0d4ba49c
Jon Lamb <lamb.jon.io@gmail.com>
2023-02-06 12:18:57 -0800

    Fixup renode PHY SSR

--- renode/cargo-runner.sh
+++ renode/cargo-runner.sh
@@ -2,14 +2,15 @@
 
 set -euo pipefail
 
-renode_bin=${RENODE_PATH-renode}
-
 bin_path=$1
+
+renode_bin=${RENODE_PATH-renode}
+renode_opts=${RENODE_OPTS-}
 
 echo "Running renode with binary '${bin_path}'"
 
 export CARGO_RENODE_BIN_RELATIVE_PATH="${bin_path}"
 
-${renode_bin} renode/emulate.resc
+${renode_bin} ${renode_opts} renode/emulate.resc
 
 exit 0
