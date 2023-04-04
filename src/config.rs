d12d52c10c74da84f92dd3f89ead5fe0e2c383ad
Jon Lamb <lamb.jon.io@gmail.com>
2023-04-04 11:34:05 -0700

    Adjust pms wake interval

--- src/config.rs
+++ src/config.rs
@@ -24,14 +24,14 @@ pub const SGP41_MEASUREMENT_INTERVAL_MS: u32 = 1000;
 pub const SHT31_MEASUREMENT_INTERVAL_MS: u32 = 2500;
 pub const S8LP_MEASUREMENT_INTERVAL_MS: u32 = 5000;
 
-/// PMS sensor is woken up for measurements every 5 minutes
+/// PMS sensor is woken up for measurements every 3 minutes
 /// to conserve lifespan, it also needs to warm up for at
 /// least 30 seconds before taking a measurement.
 ///
 /// The measurement task is run once a second to drive the
 /// wake/measurement/sleep cycle.
 pub const PMS5003_MEASUREMENT_INTERVAL_MS: u32 = 1000;
-pub const PMS5003_WAKE_INTERVAL_MS: u32 = (5 * 60) * 1000;
+pub const PMS5003_WAKE_INTERVAL_MS: u32 = (3 * 60) * 1000;
 pub const PMS5003_WARM_UP_PERIOD_MS: u32 = 45 * 1000;
 
 /// Number of measurements to perform (one per measurement interval) before
