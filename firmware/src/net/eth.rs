18b53d6745723fa61363139cf00b0401a5bdd557
Jon Lamb <lamb.jon.io@gmail.com>
2023-10-17 06:38:24 -0700

    Revert enc28j60 workarounds, my hw is flaky

--- firmware/src/net/eth.rs
+++ firmware/src/net/eth.rs
@@ -57,10 +57,6 @@ impl<'buf> Device for Eth<'buf> {
     type TxToken<'a> = TxToken<'a> where Self: 'a;
 
     fn receive(&mut self, _timestamp: Instant) -> Option<(Self::RxToken<'_>, Self::TxToken<'_>)> {
-        // TODO - reading EIR and/or ESTAT appears to resolve rx occasionally not firing?
-        let _estat = self.drv.estat().ok();
-        let _eir = self.drv.eir().ok();
-
         match self.drv.next_packet() {
             Ok(Some(packet)) => {
                 let pkt_len = packet.len() as usize;
